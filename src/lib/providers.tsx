62693717aa503aa7461cc835be4a143959a510cb
kuri884 <krc.kuribayashi@gmail.com>
2024-05-24 16:14:44 +0900

    fix: providersのlintエラーがでなくなった

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -3,7 +3,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -14,7 +13,6 @@ const queryClient = new QueryClient({
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     <QueryClientProvider client={queryClient}>
       <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
     </QueryClientProvider>
